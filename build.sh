21bccbca932ee4fd0391950e939576843035f2ce
tatsuya.nakaji <tatsuya.nakaji@gpol.co.jp>
2024-07-30 13:05:04 +0900

    GOARCH=arm64に変更

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ OUTPUT_BIN="main"
 
 # デバッグ情報、シンボルテーブル、パス情報を削除してビルド
 echo "Building with debug and symbol table removal..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath -o $OUTPUT_BIN $GO_FILE
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o $OUTPUT_BIN $GO_FILE
 
 # stripコマンドでさらに不要な情報を削除
 echo "Stripping the binary..."
@@ -19,3 +19,4 @@ echo "Compressing with UPX (lzma)..."
 upx --lzma $OUTPUT_BIN
 
 echo "Build completed. Final binary: $OUTPUT_BIN"
+strip --version
